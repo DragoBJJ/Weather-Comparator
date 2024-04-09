7e5fff3f19cc02e73389d6e8d8563540a3a36fcf
Jakub Pawelski <drago@Jakubs-MacBook-Pro.local>
2024-04-09 14:44:19 +0200

    Resolved connected issue. Added https to LOCATION_API URL

--- src/constant/api.ts
+++ src/constant/api.ts
@@ -1,3 +1,3 @@
 export const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?`;
-export const LOCATION_API = `http://api.openweathermap.org/geo/1.0/direct?q=`;
+export const LOCATION_API = `https://api.openweathermap.org/geo/1.0/direct?q=`;
 export const AIR_POLLUTION_URL = `https://api.openweathermap.org/data/2.5/air_pollution?`;
