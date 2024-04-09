053333370290b55a28b33ca290003b33b195af66
Jakub Pawelski <drago@Jakubs-MacBook-Pro.local>
2024-04-09 16:26:23 +0200

    Changed images path

--- src/types/images.ts
+++ src/types/images.ts
@@ -5,8 +5,8 @@ export type KeyType = 'Clouds' | 'Clear' | 'Haze' | 'Rain';
 export type ImagesType = Record<KeyType, string>;
 
 export const Images: ImagesType = {
-  Clouds: '/public/images/clouds.jpg',
-  Clear: '/public/images/clearSky.jpg',
-  Haze: '/public/images/haze.jpg',
-  Rain: '/public/images/rain.jpg',
+  Clouds: '/images/clouds.jpg',
+  Clear: '/images/clearSky.jpg',
+  Haze: '/images/haze.jpg',
+  Rain: '/images/rain.jpg',
 };
