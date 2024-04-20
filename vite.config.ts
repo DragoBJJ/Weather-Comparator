597d73a96f9f9ada6fd931cbb01b372618f7da81
Jakub Pawelski <drago@Jakubs-MacBook-Pro.local>
2024-04-20 22:59:41 +0200

    Added github actions flow

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv } from 'vite';
-import packageJson from './package.json';
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite';
+import { version } from './package.json';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    base: '/',
+    build: {
+      minify: true,
+      sourcemap: false,
+    },
     define: {
-      __APP_VERSION__: JSON.stringify(packageJson.version),
+      __APP_VERSION__: JSON.stringify(version),
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
     },
     server: {
       port: 3000,
     },
 
-    plugins: [react()],
+    plugins: [react(), splitVendorChunkPlugin()],
   };
 });
