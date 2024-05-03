2f63507828d8bf7660aa6a0cbdfb8d2eebef716b
Jakub Pawelski <drago@Jakubs-MacBook-Pro.local>
2024-05-03 13:31:20 +0200

    Displayed app and environment version

--- src/components/molecules/navigation/index.tsx
+++ src/components/molecules/navigation/index.tsx
@@ -18,10 +18,15 @@ export const Navigation = ({title, mode ,setMode}: NavigatorType) => {
 
     const {createLocationUrl, weatherData ,sortWeatherData} = UseWeatherAppContext();
 
+
+    const envVersion = import.meta.env.VITE_ENVIRONMENT || "";
+    const appInfo = envVersion && __APP_VERSION__ ? `App Version: ${__APP_VERSION__} & ENVIRONMENT: ${envVersion}`: ""
+
    return useMemo(()=> {
     return (
         <div className="flex flex-col w-full min-h-[210px] p-6 bg-white rounded-lg shadow-sm mb-4">
         <Typography tag="h1" textSize="2xl" textColor="blue" textPosition="center">🚀 {title}</Typography>
+        {appInfo ? <Typography tag="h1" textSize="xl" textColor="blue" textPosition="center">🚀 {appInfo}</Typography>: <></>}
         <div className="flex flex-col justify-between w-full min-h-[200px] mt-6 md:mt-0">
         <Switch<SwitchModeType> label="Mode" values={SwitchData} stateValue={mode} setStateValue={setMode} />
         {mode === 'Search' ? <WeatherSearchBar createLocationUrl={createLocationUrl}/> : <></>}
